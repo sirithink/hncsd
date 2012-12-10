213eec98a68109fc564125d507ad695e6955b06d
mangege <mr.mangege@gmail.com>
2012-12-10 21:56:12 +0800

    init

---
+++ README.markdown
@@ -0,0 +1,4 @@
+HNCSD
+===
+
+需求变更,为快速开发而换到Rails,原Sinatra简单版本请查看[old_sinatra_version](https://github.com/mangege/hncsd/tree/old_sinatra_version)分支.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hncsd::Application.config.secret_token = '1f0d43cfba8322a563aea0b1c9f9d28601ae4577cbdc50d1e7a91fa007ff71fef74b03feeb13b0bceeeb354d889b33e39dd1531ef22210c2773c52d2151b3dd3'
