76f20bf7dbff3cec7806e197403f8af9108a1f26
mangege <mr.mangege@gmail.com>
2012-12-12 23:13:57 +0800

    查询结果显示

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -36,20 +36,16 @@ class MainController < ApplicationController
       flash.alert = '请重试'
       redirect_to action: :query
     else
-      fields = params.dup
+      fields = params.slice( *params.keys.select{|k| k =~ /^(ctl00|__)/} )
       fields['ctl00$ContentPlaceHolder1$ButtonClwfOk'] = '确定'
       page = Agent.post_form(fields, session[:hncsjj_session])
-      page.body.force_encoding('utf-8')
-      check_error(page)
-      if @error.nil?
-        @title, @data = extract_data(page)
-        save_unknown_error(page)
-      end
-      if @error
-        flash.now.alert = @error 
+      query_result = QueryResult.new(page)
+      if query_result.error
+        flash.now.alert = query_result.error 
         self.query
         render 'query'
       else
+        @title, @data = query_result.data
         render 'post_query'
       end
     end
@@ -63,41 +59,4 @@ class MainController < ApplicationController
       send_data(image_io.read, type: Mime::JPEG)
     end
   end
-
-  private
-  def check_error(page)
-    body = page.body
-    if body =~ Regexp.new('你输入的验证码错误，请重新输入')
-      @error = "验证码错误，请重新输入"
-    elsif  body =~ Regexp.new('没有找到机动车')
-      @error = page.search('#labTitle').first.content
-    elsif body =~ Regexp.new('违法信息查询错误')
-      @error = page.search('#labNoPic').first.content
-    elsif body =~ Regexp.new('你访问的页面已经过期')
-      @error = "访问过期,请返回首页刷新重试"
-    end
-  end
-
-  def save_unknown_error(page)
-    if @error.nil? && @data.nil?
-      @error = "未知错误,请重试或联系技术人员"
-      file = File.join(Rails.root, "log/unknown_error_#{Time.now.to_i}.log")
-      File.write(file, page.body)
-    end
-  end
-
-  def extract_data(page)
-    data_table = page.search('#gvWZ')
-    rows = nil
-    title = (page.search('#labTitle').first.content rescue '')
-    if data_table.size > 0
-      rows = []
-      rows << data_table.search('tr').first.search('th').map{|th| th.text} #head
-      data_table.search('tr')[1..-1].each do |td_row|
-        rows << td_row.search('td').map{|th| th.text}
-      end
-    end
-    [title, rows]
-  end
-
 end
---
+++ app/models/query_result.rb
@@ -0,0 +1,48 @@
+# -*- encoding : utf-8 -*-
+class QueryResult
+  attr_reader :page, :error, :data
+  def initialize(page)
+    @page = page
+    @page.body.force_encoding('utf-8')
+
+    check_error
+    extract_data
+    save_unknown_error
+  end
+
+  private
+  def check_error
+    body = @page.body
+    if body =~ Regexp.new('你输入的验证码错误，请重新输入')
+      @error = "验证码错误，请重新输入"
+    elsif  body =~ Regexp.new('没有找到机动车')
+      @error = @page.search('#labTitle').first.content
+    elsif body =~ Regexp.new('违法信息查询错误')
+      @error = @page.search('#labNoPic').first.content
+    elsif body =~ Regexp.new('你访问的页面已经过期')
+      @error = "访问过期,请返回首页刷新重试"
+    end
+  end
+
+  def extract_data
+    data_table = @page.search('#gvWZ')
+    rows = nil
+    title = (@page.search('#labTitle').first.content rescue nil)
+    if data_table.size > 0
+      rows = []
+      rows << data_table.search('tr').first.search('th').map{|th| th.text} #head
+      data_table.search('tr')[1..-1].each do |td_row|
+        rows << td_row.search('td').map{|th| th.text}
+      end
+    end
+    @data = [title, rows]
+  end
+
+  def save_unknown_error
+    if @error.nil? && @data.first.nil?
+      @error = "未知错误,请重试或联系技术人员"
+      file = File.join(Rails.root, "log/unknown_error_#{Time.now.to_i}.log")
+      File.write(file, @page.body)
+    end
+  end
+end
